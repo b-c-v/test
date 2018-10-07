28dd5d8b3412d573d7fb6e24563384b74c031c1c
Sergey <boboshkocb@gmail.com>
2018-10-07 16:11:06 +0300

    Add method to print

--- HelloWorldTest.java
+++ HelloWorldTest.java
@@ -1,10 +1,21 @@
 package test;
 
+import java.util.Scanner;
+
 public class HelloWorldTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		myPrint("Hello ");
+
 
 	}
+
+	private static void myPrint(String hello) {
+		Scanner aScanner = new Scanner(System.in);
+		System.out.print("Enter your name: ");
+		String name = aScanner.nextLine();
+		System.out.println(hello + name);
+	}
+
 
 }
